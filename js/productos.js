2076a81ca44266a63b340782b563f74e9d3c6ea2
duban77 <159207588+duban77@users.noreply.github.com>
2025-04-09 21:21:01 +0000

    3.1

--- js/productos.js
+++ js/productos.js
@@ -5,6 +5,8 @@ fetch("https://fakestoreapi.com/products")
   .then(data => {
     productos = data;
     renderizarProductos(productos, "productos");
+    mostrarProductoDestacado();
+
     mostrarFavoritos();
   });
 
@@ -23,3 +25,16 @@ function renderizarProductos(lista, contenedorId) {
     contenedor.appendChild(div);
   });
 }
+function mostrarProductoDestacado() {
+    const random = Math.floor(Math.random() * productos.length);
+    const prod = productos[random];
+  
+    const div = document.getElementById("productoDestacado");
+    div.innerHTML = `
+      <img src="${prod.image}" alt="${prod.title}">
+      <h3>${prod.title}</h3>
+      <p>$${prod.price}</p>
+      <button onclick="agregarFavorito(${prod.id})">❤️ Agregar</button>
+    `;
+  }
+  
