598f7ef512d0c3d94a59517b579f44c0b5b9aeaf
duban77 <159207588+duban77@users.noreply.github.com>
2025-04-10 01:20:22 +0000

    5.8

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,13 +1,16 @@
-function mostrarTodos() {
-  document.getElementById("productos").style.display = "block";
-  document.getElementById("favoritos").style.display = "none";
-  renderizarProductos(productos, "productos");
-}
-
-function mostrarFavoritos() {
-  const favs = JSON.parse(localStorage.getItem("favoritos")) || [];
-  const favData = productos.filter(p => favs.includes(p.id));
-  document.getElementById("productos").style.display = "none";
-  document.getElementById("favoritos").style.display = "block";
-  renderizarProductos(favData, "favoritos");
-}
+function agregarFavorito(id) {
+    let favs = JSON.parse(localStorage.getItem("favoritos")) || [];
+    if (!favs.includes(id)) {
+      favs.push(id);
+      localStorage.setItem("favoritos", JSON.stringify(favs));
+      alert("Agregado a favoritos");
+    }
+    mostrarFavoritos();
+  }
+  
+  function mostrarFavoritos() {
+    const favs = JSON.parse(localStorage.getItem("favoritos")) || [];
+    const favData = productos.filter(p => favs.includes(p.id));
+    renderizarProductos(favData, "favoritos");
+  }
+  
