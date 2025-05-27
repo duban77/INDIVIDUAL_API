0ba7ea64144b198318e6e55c796ffab807d7e9d2
duban77 <159207588+duban77@users.noreply.github.com>
2025-05-27 02:55:14 +0000

    cambios totales

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "feriados-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./js/buscador.js",
+  "./js/favoritos.js",
+  "./js/filtro.js",
+  "./js/productos.js",
+  "./js/registro.js",
+  "./img/representativa.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
